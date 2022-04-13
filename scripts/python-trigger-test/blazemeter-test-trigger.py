6a04f2e3c5ed8ad5cedce6c534e841521884df42
tiagoms1 <tiago.ms1@gmail.com>
2022-04-13 17:55:55 -0300

    Update blazemeter-test-trigger.py
    New attempt to make github action work

--- scripts/python-trigger-test/blazemeter-test-trigger.py
+++ scripts/python-trigger-test/blazemeter-test-trigger.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # script to trigger a test and send a file when needed to be executed.
-# to execute the script you can call it as: #20200413
+# to execute the script you can call it as: #20200413 5:55pm
 # help:
 # python blazemeter-test-trigger.py -h
 #  --testid TESTID, -t TESTID
